18c6a3a95f82a0bede4c1f774c32995a8c68e30a
Celestine Assumpta <assumptachinonso42@gmail.com>
2026-01-30 11:12:35 +0100

    30/1/2026

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,114 +1,116 @@
-"use client";
+"use client"
 
-import { useState } from "react";
-import { Trash2, Check } from "lucide-react";
-
-type Todo = {
+import { CheckCircle2, Trash2Icon } from "lucide-react";
+import { useState } from "react"
+type Todos = {
   id: number;
   text: string;
-  completed: boolean;
-};
+  completed: boolean
+}
 
-export default function Home() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const [input, setInput] = useState("");
+export default function HomePage() {
 
-  const addTodo = () => {
-    if (!input.trim()) return;
+  const [addTodos, setAddTodos] = useState<Todos[]>([])
+  const [newTodos, setNewTodos] = useState("")
 
-    setTodos([
-      ...todos,
-      {
+  const handleTodos = () => {
+    if(!newTodos.trim()) return;
+
+    setAddTodos([
+      ...addTodos,{
         id: Date.now(),
-        text: input,
-        completed: false,
-      },
-    ]);
+        text: newTodos,
+        completed: false
+      }
+    ])
+    setNewTodos("")
+  }
 
-    setInput("");
-  };
+  const handleToggleTodo = (id: number) => {
+    setAddTodos(
+      addTodos.map((todo) => 
+      todo.id === id
+    ? {...todo, completed: !todo.completed}
+    : todo
+   )
+    )
+  }
 
-  const toggleTodo = (id: number) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id
-          ? { ...todo, completed: !todo.completed }
-          : todo
-      )
-    );
-  };
+  const handleDeleteTodo = (id: number) => {
+    setAddTodos(addTodos.filter((todo) => todo.id !== id))
+  }
 
-  const deleteTodo = (id: number) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
+  return(
+    <main className="min-h-screen flex items-center justify-center">
+      <div className="w-full max-w-md bg-white  rounded-2xl shadow-xl p-6">
+        <h1 className="text-2xl font-bold text-gray-500 text-center mb-3">
+          My TodoList   </h1>
+            
 
-  return (
-    <main className="min-h-screen flex items-center justify-center ">
-      <div className="w-full max-w-md bg-white rounded-2xl shadow-xl p-6">
-        <h1 className="text-2xl font-bold text-center text-gray-800 mb-6">
-          My Todo List
-        </h1>
+            {/*input section */}
+            <div className="flex gap-5 mb-6">
+              <input type="text"
+              value={newTodos}
+              onChange={(e) => setNewTodos(e.target.value)}
+              placeholder="Add a todo..."
+              className="flex-1 px-4 py-2 border border-gray-100 rounded-lg text-gray-500" />
+              <button onClick={handleTodos}
+              className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
+                Add</button>
+                       </div>
 
-        {/* Input */}
-        <div className="flex gap-2 mb-6">
-          <input
-            type="text"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            placeholder="Add a new task..."
-            className="flex-1 rounded-xl border text-2xl text-gray-900 border-gray-900 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-          />
-          <button
-            onClick={addTodo}
-            className="bg-blue-600 text-black px-4 py-2 rounded-xl hover:bg-blue-700 transition"
-          >
-            Add
-          </button>
-        </div>
+                       {/*Todo section*/}
+                       <ul className="space-y-4">
+                        {[...addTodos]
+                        .sort((a, b) =>
+                        Number(a.completed) - Number(b.completed))
+                        .map((todo) => (
+                          <li
+                          key={todo.id} 
+                          className="flex items-center uppercase justify-between bg-gray-100 rounded-2xl px-4 py-3">
+                            <span
+                            onClick={() => handleToggleTodo(todo.id)}
+                            className={`cursor-pointer flex-1 ${
+                              todo.completed 
+                              ? "line-through text-red-500"
+                              : "text-gray-800"
+                            }`}>
+                              {todo.text}
+                               </span>
+                              {/*check*/}
+                              <button
+                              onClick={() => handleToggleTodo(todo.id)}
+                              className={`mr-2 ${
+                                todo.completed
+                                ? "text-green-500 hover:tetx-green-600"
+                                : "text-gray-400 hover:text-gray-600"
+                              }`}>
+                                <CheckCircle2 size={24}/>
+                        </button>
+                        {/*deleteIcon*/}
+                        <button
+                        onClick={() => handleDeleteTodo(todo.id)}
+                        className="text-red-500 hover:text-red-600 mr-3">
+                          <Trash2Icon size={24} />
 
-        {/* Todo List */}
-        <ul className="space-y-3">
-          {todos.map((todo) => (
-            <li
-              key={todo.id}
-              className="flex items-center justify-between bg-gray-100 rounded-xl px-4 py-3"
-            >
-              <span
-                onClick={() => toggleTodo(todo.id)}
-                className={`cursor-pointer flex-1 ${
-                  todo.completed
-                    ? "line-through text-red-400"
-                    : "text-gray-800"
-                }`}
-              >
-                {todo.text}
-              </span>
+                        </button>
+                           
 
-              {/* Check Icon */}
-              <button
-                onClick={() => toggleTodo(todo.id)}
-                className="text-green-600 hover:text-green-800 mr-3"
-              >
-                <Check size={20} />
-              </button>
+                          </li>
+                        ))
+                        }
 
-              {/* Trash Icon */}
-              <button
-                onClick={() => deleteTodo(todo.id)}
-                className="text-red-500 hover:text-red-700"
-              >
-                <Trash2 size={20} />
-              </button>
-            </li>
-          ))}
-        </ul>
+                       </ul>
 
-        {todos.length === 0 && (
-          <p className="text-center text-gray-800 mt-6">
-            No tasks yet. Add one
-          </p>
-        )}
+                      {addTodos.length === 0 && (
+                        <p className="text-gray-500 hover:text-600 mt-6 flex justify-center">
+                          No todo yet? add one...
+                        </p>
+                      )}
+       
       </div>
+
+
     </main>
-  );
+  )
 }
